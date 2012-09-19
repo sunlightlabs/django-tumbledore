a71300c31436a3152ccf87482113a84b45c0de01
Dan Drinkard <dan.drinkard@gmail.com>
2012-09-18 21:28:11 -0400

    fix missing template tags, migrations, etc

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = open('README.md').read()
 setup(
     name       = __appname__,
     version    = __version__,
-    packages   = ['tumbledore', ],
+    packages   = ['tumbledore', 'tumbledore.templates', 'tumbledore.templatetags', 'tumbledore.migrations'],
 
     author       = "Dan Drinkard",
     author_email = "ddrinkard@sunlightfoundation.com",
--- tumbledore/__init__.py
+++ tumbledore/__init__.py
@@ -10,4 +10,4 @@ appname and version.
 """
 
 __appname__ = "tumbledore"
-__version__ = "0.1.5"
+__version__ = "0.1.6"
