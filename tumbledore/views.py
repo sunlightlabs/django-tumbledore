62829761f1d274edfe0fe2c9b559079aa568f3b7
Dan Drinkard <dan.drinkard@gmail.com>
2012-09-18 21:08:17 -0400

    fix order_by and comparator

--- tumbledore/views.py
+++ tumbledore/views.py
@@ -9,8 +9,8 @@ def index(request, mount_point):
     page = int(request.GET.get('page', '1'))
     tumblelog = get_object_or_404(Tumblelog, mount_on=mount_point, is_active=True)
     queryset = tumblelog.posts.all() if request.user.is_staff else tumblelog.posts.published()
-    if tumblelog.sort_posts_by is 'O':
-        queryset = queryset.order_by(['sort_order'])
+    if tumblelog.sort_posts_by == 'O':
+        queryset = queryset.order_by('sort_order')
 
     object_list = Paginator(queryset, tumblelog.posts_per_page).page(int(page))
     for obj in object_list:
